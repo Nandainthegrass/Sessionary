3ee9237b9cbe485de5116d025b21075dcb640935
Winston D'mello <dmellowinston@gmail.com>
2024-01-06 18:54:10 +0530

    Backend Base Design Done

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -1,9 +1,42 @@
 from fastapi import FastAPI
+import requests
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get('/{Number}')
-def home(Number):
-    return {"Hello": Number}
+class User(BaseModel):
+    name: str
+    password: str
 
-#Nanda You Suck
+class Message(BaseModel):
+    data: str
+    sender_id: int
+    TimeStamp: str
+
+
+@app.post('/register_user/{user_id}/')
+def register_user(user_id: int, user: User):
+    return None
+
+@app.get('/load_details/{session_id}')
+def Load_Session_Details(session_id: int):
+    return {
+        session_id : {
+            "UserX 1": "Message",
+            "UserY 1": "Message",
+            "UserX 2": "Message"
+        }
+    }
+
+@app.post('/message_sent/{Session_id}')
+def Send_Messages(Sessage_id: int, message: Message):
+    return None
+
+@app.get('/message_recieve/{Session_id}')
+def Recieve_Messages(Session_id: int):
+    return {
+        "Message": "Data",
+        "TimeStamp": "Time",
+        "Sender": "Name"
+    }
