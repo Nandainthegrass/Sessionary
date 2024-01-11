64eda44c7f073f1db18ae9f728d57c8d05e34e6d
Winston D'mello <dmellowinston@gmail.com>
2024-01-11 10:02:03 +0530

    Jwt_Create_Tokens Added

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -1,12 +1,22 @@
-from fastapi import FastAPI, Response, WebSocket
+from fastapi import FastAPI, Response, WebSocket, Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
 import json
+from datetime import datetime, timedelta
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
+from passlib.context import CryptContext
+from jose import JWTError, jwt
 from models import User, Message, Session
 from database import Users
 from nanoid import generate
 
 app = FastAPI()
+
+SECRET_KEY = "35fd6282e2e929ad65cb27564ee7dd71884928de60a516573eb6cd1bbdac9ec6"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 origins = [
     "http://localhost:5173",
@@ -28,33 +38,49 @@ async def register_user(user: User):
         await Users.insert_one({
             "id": id,
             "Username": user.username,
-            "Password": user.password,
+            "Password": pwd_context.hash(user.password),
             "Status": "Offline"
         })
         content = {"UserID": id, "Username":user.username}
         return Response(status_code=200)
     else:
         return Response(status_code=409)#Username taken
-    
+
+def create_access_token(UserID: str):
+    token_data = {"sub": UserID}
+    expires_delta = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.utcnow() + expires_delta
+    to_encode = {"exp": expire, **token_data}
+    jwt_token = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return jwt_token
+
 @app.post('/Login/')
 async def login_user(user:User):
     check = await Users.find_one({"Username":f"{user.username}"})
     if check == None:
         return Response(status_code=400)#User Doesn't Exist/ Invalid User
     else:
-        if user.password == check['Password']:
+        if pwd_context.verify(user.password, check['Password']):
+            jwt_token = create_access_token(check['id'])
+
             content = {"UserID": check['id'], "Username":check['Username']}
             return Response(content = json.dumps(content), status_code=200, headers={
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {jwt_token}'
             })
         else:
             return Response(status_code=401)#Password doesn't match
-@app.get('/Load_User_Details/{UserID}')
-async def Load_User(UserID: str):
-    pass
 
 @app.websocket("/connection/{UserID}")
-async def connection(UserID: str, websocket: WebSocket):
+async def connection(UserID: str, websocket: WebSocket, token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: str = payload.get('sub')
+        if user_id != UserID:
+            raise HTTPException(status_code=400, detail='Invalid User, Access Denied!')
+    except JWTError:
+        raise HTTPException(status_code=401, detail='Invalid Credentials')
+    
     await websocket.accept()
     while True:
         data = await websocket.receive_text()
