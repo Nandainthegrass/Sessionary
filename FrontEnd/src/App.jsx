8c0266613b7b7e1a71ee7033cbb1c34d9a84503c
Nandainthegrass <postnanda200@gmail.com>
2024-06-25 15:37:15 +0530

    Routing fix one

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes, Navigate } from "react-router-dom";
+import { Route, Routes, Navigate, Router } from "react-router-dom";
 import Signup from "./components/Signup/Signup";
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
@@ -18,11 +18,11 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Navigate to="/home" />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/message" element={<Message />} />
+        <Route path="/" element={<Navigate to="/Sessionary/home" />} />
+        <Route path="/Sessionary/home" element={<Home />} />
+        <Route path="/Sessionary/signup" element={<Signup />} />
+        <Route path="/Sessionary/login" element={<Login />} />
+        <Route path="/Sessionary/message" element={<Message />} />
 
         <Route path="*" element={<NotFound />} />
       </Routes>
