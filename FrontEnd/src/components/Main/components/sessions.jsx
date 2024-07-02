5eee2bc404aa6c5ebce69b1e4a8b162a9feccb80
Winston-Dmello <116348041+Winston-Dmello@users.noreply.github.com>
2024-07-02 23:28:58 +0530

    Update sessions.jsx added UseEffect Import

--- FrontEnd/src/components/Main/components/sessions.jsx
+++ FrontEnd/src/components/Main/components/sessions.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/sessions.css";
 import profile from "./Session_Images/Profile.png";
 import ContextMenu from "./ContextMenu";
